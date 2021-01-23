8ec70bcc2d03c64da0eb4b25b68388b786c76c9e
Taras Shumeiko <shumeyko@gmail.com>
2021-01-23 15:19:45 +0200

    add ParseToIntSlice & URL methods

--- convert.go
+++ convert.go
@@ -1,7 +1,9 @@
 package str
 
 import (
+	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -10,9 +12,29 @@ var (
 	matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
 )
 
+func (s Str) Url() *url.URL{
+	if v, err := url.ParseRequestURI(s.value); err == nil {
+		return v
+	}
+	return nil
+}
+
 func (s Str) CamelToSnakeCase() Str {
 	s.value = matchFirstCap.ReplaceAllString(s.value, "${1}_${2}")
 	s.value = matchAllCap.ReplaceAllString(s.value, "${1}_${2}")
 	s.value = strings.ToLower(s.value)
 	return s
+}
+
+func (s Str) ParseToIntSlice(separator string) (result []int) {
+	result = make([]int, 0)
+	if len(separator) == 0 {
+		return
+	}
+	for _, v := range strings.Split(s.value, separator) {
+		if n, err := strconv.Atoi(v); err == nil {
+			result = append(result, n)
+		}
+	}
+	return
 }
